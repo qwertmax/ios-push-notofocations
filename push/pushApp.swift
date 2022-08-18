a5e6757950de61a173ba008bfd70072ce30160c6
Maxim Tishchenko <qwertmax@gmail.com>
2022-08-18 09:30:16 +0200

    push

---
+++ push/pushApp.swift
@@ -0,0 +1,65 @@
+//
+//  pushApp.swift
+//  push
+//
+//  Created by Maxim Tishchenko on 17.08.2022.
+//
+
+import SwiftUI
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        registerForPushNotifications()
+        print("registered")
+        getNotificationSettings()
+
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+
+    func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+      print("Failed to register: \(error)")
+    }
+}
+
+@main
+struct pushApp: App {
+    // inject into SwiftUI life-cycle via adaptor !!!
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
+
+func registerForPushNotifications() {
+  UNUserNotificationCenter.current()
+    .requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+      print("Permission granted: \(granted)")
+    }
+}
+
+func getNotificationSettings() {
+  UNUserNotificationCenter.current().getNotificationSettings { settings in
+//    print("Notification settings: \(settings)")
+      
+    guard settings.authorizationStatus == .authorized else { return }
+    DispatchQueue.main.async {
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+  }
+}
